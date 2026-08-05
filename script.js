0b9aedd5f628fee50994011c48b193b438594a75
Van1ti <niceboyvova@gmail.com>
2026-08-05 10:38:28 +0200

    commit

--- script.js
+++ script.js
@@ -24,3 +24,52 @@ document.addEventListener('DOMContentLoaded', () => {
 
 //modal
 
+
+const openModalBtn = document.getElementById('open-modal-btn')
+const modalOverlay = document.getElementById('modal_overlay')
+const modal1 = document.getElementById('modal-1')
+const modal2 = document.getElementById('modal-2')
+
+const saveNameBtn = document.getElementById('save-name-btn')
+const userNameInput = document.getElementById('user-name-input')
+const closeButtons = document.querySelectorAll('.close-btn')
+
+
+function closeModal() {
+  modalOverlay.classList.add('modal_overlay--hidden')
+  modal1.classList.remove('hidden')
+  modal2.classList.add('hidden')
+}
+
+openModalBtn.addEventListener('click', () => {
+  modalOverlay.classList.remove('modal_overlay--hidden')
+  modal1.classList.remove('hidden')
+  modal2.classList.add('hidden')
+})
+
+saveNameBtn.addEventListener('click', () => {
+  const name = userNameInput.value.trim()
+
+  if (name !== '') {
+
+    openModalBtn.textContent = `Вітаємо, ${name}!`
+
+    modal1.classList.add('hidden')
+    modal2.classList.remove('hidden')
+
+    userNameInput.value = ''
+  } else {
+    alert("Будь ласка, введіть своє ім'я!")
+  }
+})
+
+closeButtons.forEach(btn => {
+  btn.addEventListener('click', closeModal)
+})
+
+
+modalOverlay.addEventListener('click', (event) => {
+  if (event.target === modalOverlay) {
+    closeModal()
+  }
+})
