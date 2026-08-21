851b07160fa145e519df1dc7f1c86dc9ea4fe77a
Van1ti <niceboyvova@gmail.com>
2026-08-21 18:26:25 +0200

    commit

--- script.js
+++ script.js
@@ -23,184 +23,187 @@ document.addEventListener('DOMContentLoaded', () => {
 })
 
 //modal
-const openModalBtn = document.getElementById('open-modal-btn')
-const modalOverlay = document.getElementById('modal_overlay')
-const modal1 = document.getElementById('modal-1')
+document.addEventListener('DOMContentLoaded', () => {
+  const openModalBtn = document.getElementById('open-modal-btn')
+  const modalOverlay = document.getElementById('modal_overlay')
+  const modal1 = document.getElementById('modal-1')
 
-const saveNameBtn = document.getElementById('save-name-btn')
-const userNameInput = document.getElementById('user-name-input')
-const closeButtons = document.querySelectorAll('.close-btn')
+  const saveNameBtn = document.getElementById('save-name-btn')
+  const userNameInput = document.getElementById('user-name-input')
+  const closeButtons = document.querySelectorAll('.close-btn')
 
-function closeModal() {
-  modalOverlay.classList.add('modal_overlay--hidden')
-}
-
-openModalBtn.addEventListener('click', () => {
-  modalOverlay.classList.remove('modal_overlay--hidden')
-})
-
-saveNameBtn.addEventListener('click', () => {
-  const name = userNameInput.value.trim()
-
-  if (name !== '') {
-
-    openModalBtn.textContent = `Вітаємо, ${name}!`
-
-    userNameInput.value = ''
-
-    closeModal()
-  } else {
-    alert("Будь ласка, введіть своє ім'я!")
+  function closeModal() {
+    modalOverlay.classList.add('modal_overlay--hidden')
   }
-})
 
-closeButtons.forEach(btn => {
-  btn.addEventListener('click', closeModal)
-})
+  openModalBtn.addEventListener('click', () => {
+    modalOverlay.classList.remove('modal_overlay--hidden')
+  })
 
-window.addEventListener('keydown', (event) => {
-  if (event.key === 'Escape' && !modalOverlay.classList.contains('modal_overlay--hidden')) {
-    closeModal()
-  }
-})
+  saveNameBtn.addEventListener('click', () => {
+    const name = userNameInput.value.trim()
 
-modalOverlay.addEventListener('click', (event) => {
-  if (event.target === modalOverlay) {
-    closeModal()
-  }
+    if (name !== '') {
+
+      openModalBtn.textContent = `Вітаємо, ${name}!`
+
+      userNameInput.value = ''
+
+      closeModal()
+    } else {
+      alert("Будь ласка, введіть своє ім'я!")
+    }
+  })
+
+  closeButtons.forEach(btn => {
+    btn.addEventListener('click', closeModal)
+  })
+
+  window.addEventListener('keydown', (event) => {
+    if (event.key === 'Escape' && !modalOverlay.classList.contains('modal_overlay--hidden')) {
+      closeModal()
+    }
+  })
+
+  modalOverlay.addEventListener('click', (event) => {
+    if (event.target === modalOverlay) {
+      closeModal()
+    }
+  })
 })
 
 //year
+document.addEventListener('DOMContentLoaded', () => {
+  const yearInput = document.getElementById('year-input')
+  const resultText = document.getElementById('year-result')
+  const searchBtn = document.querySelector('.main_year--container--search--box--btn')
 
-const yearInput = document.getElementById('year-input')
-const resultText = document.getElementById('year-result')
-const searchBtn = document.querySelector('.main_year--container--search--box--btn')
+  resultText.textContent = ''
 
-resultText.textContent = ''
+  function checkLeapYear() {
+    const year = parseInt(yearInput.value.trim(), 10);
 
-function checkLeapYear() {
-  const year = parseInt(yearInput.value.trim(), 10);
+    if (isNaN(year) || year <= 0) {
+      resultText.textContent = 'Будь ласка, введіть  рік!'
+      resultText.style.color = '#990000'
+      return
+    }
 
-  if (isNaN(year) || year <= 0) {
-    resultText.textContent = 'Будь ласка, введіть  рік!'
-    resultText.style.color = '#990000'
-    return
+
+    const isLeap = (year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0)
+
+    if (isLeap) {
+      resultText.textContent = 'Ви народилися у високосний рік!'
+      resultText.style.color = '#039900'
+    } else {
+      resultText.textContent = 'Ви народилися не у високосний рік!'
+      resultText.style.color = '#990000'
+    }
   }
 
+  searchBtn.addEventListener('click', checkLeapYear)
 
-  const isLeap = (year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0)
-
-  if (isLeap) {
-    resultText.textContent = 'Ви народилися у високосний рік!'
-    resultText.style.color = '#039900'
-  } else {
-    resultText.textContent = 'Ви народилися не у високосний рік!'
-    resultText.style.color = '#990000'
-  }
-}
-
-searchBtn.addEventListener('click', checkLeapYear)
-
-yearInput.addEventListener('keypress', (event) => {
-  if (event.key === 'Enter') {
-    checkLeapYear()
-  }
+  yearInput.addEventListener('keypress', (event) => {
+    if (event.key === 'Enter') {
+      checkLeapYear()
+    }
+  })
 })
 
-
 //gamble
-const form = document.getElementById('gamble-form')
-const userInput = document.getElementById('gamble-input')
-const result = document.getElementById('gamble-result')
+document.addEventListener('DOMContentLoaded', () => {
+  const form = document.getElementById('gamble-form')
+  const userInput = document.getElementById('gamble-input')
+  const result = document.getElementById('gamble-result')
 
-form.addEventListener('submit', (event) => {
+  form.addEventListener('submit', (event) => {
 
-  event.preventDefault()
+    event.preventDefault()
 
-  const secretNumber = Math.floor(Math.random() * 10) + 1
-  const userGuess = Number(userInput.value)
+    const secretNumber = Math.floor(Math.random() * 10) + 1
+    const userGuess = Number(userInput.value)
 
-  if (!userInput.value.trim()) {
-    result.textContent = 'Будь ласка, введіть число!'
-    result.style.color = 'black'
-    return
-  }
+    if (!userInput.value.trim()) {
+      result.textContent = 'Будь ласка, введіть число!'
+      result.style.color = 'black'
+      return
+    }
 
-  if (userGuess === secretNumber) {
-    result.textContent = `Вітаю, ви вгадали число ${secretNumber}!`
-    result.style.color = '#039900'
-  } else {
-    result.textContent = `Ви програли, комп’ютер загадав ${secretNumber}`
-    result.style.color = '#990000'
-  }
+    if (userGuess === secretNumber) {
+      result.textContent = `Вітаю, ви вгадали число ${secretNumber}!`
+      result.style.color = '#039900'
+    } else {
+      result.textContent = `Ви програли, комп’ютер загадав ${secretNumber}`
+      result.style.color = '#990000'
+    }
+  })
 })
 
 //rps
-
-let userScore = 0;
-let pcScore = 0;
-
-
-const countUserEl = document.getElementById('count-user')
-const countPcEl = document.getElementById('count-pc')
-const resultEl = document.getElementById('rps-result')
-const pcButtonEl = document.getElementById('choise-pc')
-
-const rockImg = document.getElementById('choise-rock')
-const scissorsImg = document.getElementById('choise-scissors')
-const paperImg = document.getElementById('choise-paper')
-
-const choices = ['rock', 'scissors', 'paper']
+document.addEventListener('DOMContentLoaded', () => {
+  let userScore = 0;
+  let pcScore = 0;
 
 
-const choicesUA = {
-  rock: 'Камінь',
-  scissors: 'Ножиці',
-  paper: 'Папір'
-};
+  const countUserEl = document.getElementById('count-user')
+  const countPcEl = document.getElementById('count-pc')
+  const resultEl = document.getElementById('rps-result')
+  const pcButtonEl = document.getElementById('choise-pc')
 
-countUserEl.textContent = `Ви: ${userScore}`
-countPcEl.textContent = `Комп'ютер: ${pcScore}`
-resultEl.textContent = 'Зробіть свій вибір!'
+  const rockImg = document.getElementById('choise-rock')
+  const scissorsImg = document.getElementById('choise-scissors')
+  const paperImg = document.getElementById('choise-paper')
 
-
-function getPcChoice() {
-  const randomIndex = Math.floor(Math.random() * choices.length)
-  return choices[randomIndex]
-}
+  const choices = ['rock', 'scissors', 'paper']
 
 
-function playGame(userChoice) {
-  const pcChoice = getPcChoice()
+  const choicesUA = {
+    rock: 'Камінь',
+    scissors: 'Ножиці',
+    paper: 'Папір'
+  };
+
+  countUserEl.textContent = `Ви: ${userScore}`
+  countPcEl.textContent = `Комп'ютер: ${pcScore}`
+  resultEl.textContent = 'Зробіть свій вибір!'
 
 
-  pcButtonEl.textContent = `Комп'ютер обрав: ${choicesUA[pcChoice]}`
-
-  if (userChoice === pcChoice) {
-    resultEl.textContent = 'Нічия!'
-    resultEl.style.color = '#000000'
-  } else if (
-    (userChoice === 'rock' && pcChoice === 'scissors') ||
-    (userChoice === 'scissors' && pcChoice === 'paper') ||
-    (userChoice === 'paper' && pcChoice === 'rock')
-  ) {
-    userScore++;
-    countUserEl.textContent = `Ви: ${userScore}`
-    resultEl.textContent = 'Ви виграли раунд! '
-    resultEl.style.color = '#039900'
-  } else {
-    pcScore++;
-    countPcEl.textContent = `Комп'ютер: ${pcScore}`
-    resultEl.textContent = 'Комп’ютер виграв раунд! '
-    resultEl.style.color = '#c62828'
+  function getPcChoice() {
+    const randomIndex = Math.floor(Math.random() * choices.length)
+    return choices[randomIndex]
   }
-}
-
-rockImg.addEventListener('click', () => playGame('rock'))
-scissorsImg.addEventListener('click', () => playGame('scissors'))
-paperImg.addEventListener('click', () => playGame('paper'))
 
 
+  function playGame(userChoice) {
+    const pcChoice = getPcChoice()
+
+
+    pcButtonEl.textContent = `Комп'ютер обрав: ${choicesUA[pcChoice]}`
+
+    if (userChoice === pcChoice) {
+      resultEl.textContent = 'Нічия!'
+      resultEl.style.color = '#000000'
+    } else if (
+      (userChoice === 'rock' && pcChoice === 'scissors') ||
+      (userChoice === 'scissors' && pcChoice === 'paper') ||
+      (userChoice === 'paper' && pcChoice === 'rock')
+    ) {
+      userScore++;
+      countUserEl.textContent = `Ви: ${userScore}`
+      resultEl.textContent = 'Ви виграли раунд! '
+      resultEl.style.color = '#039900'
+    } else {
+      pcScore++;
+      countPcEl.textContent = `Комп'ютер: ${pcScore}`
+      resultEl.textContent = 'Комп’ютер виграв раунд! '
+      resultEl.style.color = '#c62828'
+    }
+  }
+
+  rockImg.addEventListener('click', () => playGame('rock'))
+  scissorsImg.addEventListener('click', () => playGame('scissors'))
+  paperImg.addEventListener('click', () => playGame('paper'))
+})
 
 //calculator
 
@@ -531,4 +534,98 @@ document.addEventListener('DOMContentLoaded', () => {
 
   nextBtn.addEventListener('click', () => showSlide(currentIndex + 1))
   prevBtn.addEventListener('click', () => showSlide(currentIndex - 1))
+})
+
+//sort
+document.addEventListener('DOMContentLoaded', () => {
+  const scientists = [
+    { name: "Albert", surname: "Einstein", born: 1879, dead: 1955, id: 1 },
+    { name: "Isaac", surname: "Newton", born: 1643, dead: 1727, id: 2 },
+    { name: "Galileo", surname: "Galilei", born: 1564, dead: 1642, id: 3 },
+    { name: "Marie", surname: "Curie", born: 1867, dead: 1934, id: 4 },
+    { name: "Johannes", surname: "Kepler", born: 1571, dead: 1630, id: 5 },
+    { name: "Nicolaus", surname: "Copernicus", born: 1473, dead: 1543, id: 6 },
+    { name: "Max", surname: "Planck", born: 1858, dead: 1947, id: 7 },
+    { name: "Katherine", surname: "Blodgett", born: 1898, dead: 1979, id: 8 },
+    { name: "Ada", surname: "Lovelace", born: 1815, dead: 1852, id: 9 },
+    { name: "Sarah E.", surname: "Goode", born: 1855, dead: 1905, id: 10 },
+    { name: "Lise", surname: "Meitner", born: 1878, dead: 1968, id: 11 },
+    { name: "Hanna", surname: "Hammarström", born: 1829, dead: 1909, id: 12 }
+  ];
+
+  const cardsContainer = document.querySelector('.main_sort--container--cards')
+  const buttonsContainer = document.querySelector('.main_sort--container--buttons')
+
+
+  function renderScientists(arrayToRender) {
+    cardsContainer.innerHTML = ''
+    const data = Array.isArray(arrayToRender) ? arrayToRender : [arrayToRender]
+
+    data.forEach(scientist => {
+      const card = document.createElement('div')
+      card.classList.add('scientist-card')
+      card.innerHTML = `
+            <h3>${scientist.name} ${scientist.surname}</h3>
+            <p>Роки: ${scientist.born} - ${scientist.dead}</p>
+            <p>Прожив років: ${scientist.dead - scientist.born}</p>
+        `;
+      cardsContainer.appendChild(card)
+    })
+  }
+
+  renderScientists(scientists)
+
+  const buttonsData = [
+    {
+      text: "Які вчені народилися в 19 ст.",
+      action: () => scientists.filter(s => s.born >= 1801 && s.born <= 1900)
+    },
+    {
+      text: "Знайти рік народження Albert Einstein",
+      action: () => scientists.find(s => s.name === "Albert" && s.surname === "Einstein")
+    },
+    {
+      text: "Відсортувати вчених за алфавітом",
+      action: () => [...scientists].sort((a, b) => a.surname.localeCompare(b.surname))
+    },
+    {
+      text: "Знайти вчених, прізвища яких починаються на 'C'",
+      action: () => scientists.filter(s => s.surname.startsWith("C"))
+    },
+    {
+      text: "Відсортувати за кількістю прожитих років",
+      action: () => [...scientists].sort((a, b) => (a.dead - a.born) - (b.dead - b.born))
+    },
+    {
+      text: "Видалити всіх вчених, ім'я яких починається на 'А'",
+      action: () => scientists.filter(s => !s.name.startsWith("A"))
+    },
+    {
+      text: "Знайти вченого, який народився найпізніше",
+      action: () => scientists.reduce((latest, s) => s.born > latest.born ? s : latest)
+    },
+    {
+      text: "Найдовше і найменше прожив",
+      action: () => {
+        const sorted = [...scientists].sort((a, b) => (b.dead - b.born) - (a.dead - a.born));
+        return [sorted[0], sorted[sorted.length - 1]]
+      }
+    },
+    {
+      text: "Співпадають перші літери імені і прізвища",
+      action: () => scientists.filter(s => s.name[0].toLowerCase() === s.surname[0].toLowerCase())
+    }
+  ]
+
+  buttonsData.forEach(item => {
+    const button = document.createElement('button')
+    button.textContent = item.text
+
+    button.addEventListener('click', () => {
+      const result = item.action()
+      renderScientists(result)
+    })
+
+    buttonsContainer.appendChild(button)
+  })
 })
